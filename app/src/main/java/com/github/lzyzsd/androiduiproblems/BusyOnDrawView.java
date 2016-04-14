946e90aff694b6609d7046ec223d5d326cbe9f44
falling <zyz635857938@gmail.com>
2016-04-14 12:36:06 +0800

    解决DrawOnDraw的卡顿问题

--- app/src/main/java/com/github/lzyzsd/androiduiproblems/BusyOnDrawView.java
+++ app/src/main/java/com/github/lzyzsd/androiduiproblems/BusyOnDrawView.java
@@ -6,8 +6,13 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 import android.view.View;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * Created by bruce on 2/27/16.
@@ -15,6 +20,7 @@ import android.view.View;
 public class BusyOnDrawView extends View {
 
     private Paint mPaint = new Paint();
+    private ExecutorService pool = Executors.newCachedThreadPool();
 
     public BusyOnDrawView(Context context) {
         this(context,null);
@@ -37,14 +43,24 @@ public class BusyOnDrawView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        for (int i = 0; i < 1000; i++) {
-            System.out.println("canvas = [" + canvas + "]" + i);
-        }
+        pool.execute(getCommand(canvas));
 
         mPaint.setColor(Color.RED);
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeWidth(4);
         int radius = Math.min(getWidth(), getHeight()) / 2;
         canvas.drawCircle(getWidth()/2, getHeight()/2, radius, mPaint);
+    }
+
+    @NonNull
+    private Runnable getCommand(final Canvas canvas) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    System.out.println("canvas = [" + canvas + "]" + i);
+                }
+            }
+        };
     }
 }
